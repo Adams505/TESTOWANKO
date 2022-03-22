526331cd9a94722b29f88c27e4194c268a786d86
Adams505 <101186913+Adams505@users.noreply.github.com>
2022-03-22 10:29:14 +0000

    Usuniecie scana a i b

--- Main.java
+++ Main.java
@@ -2,12 +2,11 @@ import java.util.Scanner;
 
 class Main{
 public static void main(String[]args){
-Scanner scana = new Scanner(System.in);
+Scanner scan = new Scanner(System.in);
   System.out.println("Podaj liczbe a");
- int a = scana.nextInt();
-  Scanner scanb = new Scanner(System.in);
+ int a = scan.nextInt();
   System.out.println("Podaj liczbe b");
- int b = scanb.nextInt();
+ int b = scan.nextInt();
   System.out.println("a="+a);
   System.out.println("b="+b);
   int suma=a+b;
